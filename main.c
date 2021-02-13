6d4f0710755a3ac364de0bb3448e72e6f0bb92f5
Achmad Fadhitya Muharram <afadhitya@gmail.com>
2021-02-13 20:32:14 +0700

    Fix insert

--- main.c
+++ main.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
 struct node {
 	struct node* parent;
 	int leftVal;
@@ -16,6 +14,7 @@ struct node {
 } *root; 
 
 
+// ------- Interfaces -----------------------
 int search(int key);
 int hasChild(struct node *curr);
 struct node* createNode();
@@ -27,30 +26,72 @@ void attachChildToEachNode(struct node *curr, struct node *newNode);
 void attachChildNeighborBecomeChild(struct node *curr);
 void doBalancingAfterInsert(struct node * curr);
 void doInsertion();
+void inorderTraversal(struct node *treeNode);
+void menu();
+// --------- End of Interfaces ---------------
 
 int main(int argc, char *argv[]) {
 	do {
-		char pilihan; 
+		inorderTraversal(root);
+		int pilihan; 
 		int angka;
-		scanf("%c", &pilihan);
-		fflush(stdin);
+		menu();
+		scanf("%d", &pilihan);
 		printf("\n\n");
 		switch(pilihan){
-			case '1':
+			case 1:
 				case1:
 					doInsertion();
 			break;
-			case '2':
+			case 2:
 				// case2:
 				// 	printf("Masukkan angka : ");
 				// 	scanf("%d", &angka);
 				// 	fflush(stdin);
 				// 	searchDelete(angka, root);
 			break;
-			case '3':
+			case 3:
 			break;
 		}
 	} while(1);
+	return 0;
+}
+
+void menu() {
+	printf("\n1. Insert");
+	printf("\n2. Delete");
+	printf("\nPilih: ");
+}
+
+void inorderTraversal(struct node *treeNode) {
+	if (treeNode == NULL) {
+		return;
+	}
+	if (hasTwoValue(treeNode)) {
+		inorderTraversal(treeNode->leftChild);
+		printf("l-two-%d  ", treeNode->leftVal);
+		inorderTraversal(treeNode->midChild);
+		printf("r-two-%d  ", treeNode->rightVal);
+		inorderTraversal(treeNode->rightChild);
+	}
+	if (hasOneValue(treeNode)) {
+		inorderTraversal(treeNode->leftChild);
+		printf("l-One-%d  ", treeNode->leftVal);
+		inorderTraversal(treeNode->rightChild);
+	}
+}
+
+int hasTwoValue(struct node *treeNode) {
+	if (treeNode->leftVal != NULL && treeNode->rightVal != NULL) {
+		return 1;
+	} 
+	return 0;
+}
+
+int hasOneValue(struct node *treeNode) {
+	if (treeNode->leftVal != NULL && treeNode->rightVal == NULL) {
+		return 1;
+	} 
 	return 0;
 }
 
@@ -61,18 +102,27 @@ void doInsertion() {
 	
 	struct node *leaf = findLeaf(value, root);
 	insert(value, leaf);
-//	doBalancingAfterInsert(leaf);
+	doBalancingAfterInsert(leaf != NULL ? leaf : root);
 }
 
 struct node* createNode() {
-	return (struct node*)malloc(sizeof(struct node));
+	struct node* newNode = (struct node*)malloc(sizeof(struct node));
+	newNode->leftVal = NULL;
+	newNode->rightVal = NULL;
+	newNode->midVal = NULL;
+	newNode->leftChild = NULL;
+	newNode->midChild = NULL;
+	newNode->rightChild = NULL;
+	newNode->parent = NULL;
+	newNode->neighborNode = NULL;
+	return;
 }
 
 struct node *findLeaf(int key, struct node* curr) {
-	if (curr == root) {
+	if (curr == NULL) {
 		return curr;
 	}
-	if (curr->rightChild == NULL & curr->leftChild == NULL && curr->midChild == NULL) {
+	if (curr->rightChild == NULL && curr->leftChild == NULL && curr->midChild == NULL) {
 		return curr;
 	} else if (curr->leftVal != NULL && curr->rightVal != NULL) {
 		if (curr->leftVal > key) {
@@ -111,14 +161,14 @@ void addToAvailableNode(struct node *availableNode, int key) {
 		availableNode->rightVal = key;
 		if (availableNode->leftVal >= availableNode->rightVal) {
 			int temp = availableNode->leftVal;
-			availableNode->rightVal = availableNode->leftVal;
 			availableNode->leftVal = availableNode->rightVal;
+			availableNode->rightVal = temp;
 		}
 	} else {
 		availableNode->midVal = key;
 		if (availableNode->midVal < availableNode->leftVal) {
 			int temp = availableNode->leftVal;
-			availableNode->leftVal = availableNode->leftVal;
+			availableNode->leftVal = availableNode->midVal;
 			availableNode->midVal = temp;
 		} else if (availableNode->midVal > availableNode->rightVal) {
 			int temp = availableNode->midVal;
@@ -132,11 +182,7 @@ void insert(int key, struct node* curr) {
 	if (root == NULL) {
 		curr = createNode();
 		curr->leftVal = key;
-		curr->rightVal = NULL;
-		curr->midVal = NULL;
-		curr->leftChild = NULL;
-		curr->midChild = NULL;
-		curr->rightChild = NULL;
+		root = curr;
 	} else {
 		addToAvailableNode(curr, key);
 	}
@@ -150,27 +196,44 @@ int nodeNormal(struct node* curr) {
 }
 
 void doBalancingAfterInsert(struct node * curr) {
+	if (curr == NULL) {
+		return;
+	}
 	if (nodeNormal(curr) && childHasNeighbor(curr)) {
 		attachChildsNeighborBecomeChild(curr);
 		return;
 	}
+	if (nodeNormal(curr)) {
+		return;
+	}
 	raiseMidValueToParent(curr);
 	separateNode(curr);
-	doBalancingAfterInsert(curr);
+	doBalancingAfterInsert(curr->parent);
 }
 
 int childHasNeighbor(struct node* curr) {
-	if (curr->leftChild->neighborNode != NULL || curr->rightChild->neighborNode != NULL || curr->leftChild->neighborNode != NULL) {
+	if (curr->leftChild == NULL && curr->rightChild == NULL && curr->midChild == NULL) {
+		return 0;
+	}
+	if ((curr->leftChild != NULL && curr->leftChild->neighborNode != NULL) 
+			|| (curr->midChild != NULL && curr->midChild->neighborNode != NULL) 
+			|| (curr->rightChild != NULL && curr->rightChild->neighborNode != NULL)) {
 		return 1;
 	}
 }
 
 void attachChildsNeighborBecomeChild(struct node *curr) {
-	if (curr->leftChild->neighborNode != NULL) {
-		curr->midChild = curr->leftChild->neighborNode;
-	} else {
-		curr->midChild = curr->rightChild;
-		curr->rightChild = curr->rightChild->neighborNode;
+	if (hasOneValue(curr)) {
+			curr->rightChild = curr->leftChild->neighborNode;
+	} else if (hasTwoValue(curr)) {
+		if (curr->leftChild->neighborNode != NULL) {
+			curr->midChild = curr->leftChild->neighborNode;
+			curr->leftChild->neighborNode = NULL;
+		} else {
+			curr->midChild = curr->rightChild;
+			curr->rightChild = curr->rightChild->neighborNode;
+			curr->rightChild->neighborNode = NULL;
+		}
 	}
 }
 
@@ -190,13 +253,16 @@ void attachChildToEachNode(struct node *curr, struct node *newNode) {
 		newNode->leftChild = curr->midChild;
 		newNode->rightChild = curr->rightChild;
 		curr->rightChild = curr->leftChild->neighborNode;
+		curr->leftChild->neighborNode = NULL;
 	} else if (curr->midChild->neighborNode != NULL) {
 		newNode->leftChild = curr->midChild->neighborNode;
 		newNode->rightChild = curr->rightChild;
+		curr->midChild->neighborNode = NULL;
 		curr->rightChild = curr->midChild;
 	} else {
 		newNode->leftChild = curr->rightChild;
 		newNode->rightChild = curr->rightChild->neighborNode;
+		curr->rightChild->neighborNode = NULL;
 		curr->rightChild = curr->midChild;
 	}
 	curr->midChild = NULL;
@@ -215,12 +281,12 @@ void raiseMidValueToParent(struct node *curr) {
 	if (parentNode == NULL) {
 		struct node *newNode = createNode();
 		newNode->leftVal = curr->midVal;
-		newNode->rightVal = NULL;
-		newNode->midVal = NULL;
+		curr->midVal = NULL;
 		newNode->leftChild = curr;
-		newNode->midChild = NULL;
-		newNode->rightChild = NULL;	
+		curr->parent = newNode;
+		root = newNode;
 	} else {
 		addToAvailableNode(parentNode, curr->midVal);
+		curr->midVal = NULL;
 	} 
 }
